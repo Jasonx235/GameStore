e35e685f713a6d23fb86e626933981e2cd192107
Jason Mui <Jasonmui22@gmail.com>
2020-11-08 16:47:09 -0500

    added variables to be used

--- view/signup.php
+++ view/signup.php
@@ -1,30 +1,51 @@
 <?php
+
+    //Sessions needs to be added
+
   $errors = [];
-  $bool = false;
-	if(isset($_POST['submit'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
 
-    $cleanUsername = htmlspecialchars($username);
-    $cleanPassword = htmlspecialchars($password);
-
-    $pattern = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$^";
-
-    if(empty($cleanUsername)) {
-      $errors['username'] = "Username Input Required";
+    function cleanInput($data){ //sanitize data 
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
-    if(empty($cleanPassword)) {
+	if(isset($_POST['submit'])){
+    $firstname = cleanInput($_POST['firsname']);
+    $lastname = cleanInput($_POST['lastname']);
+    $phoneNumber = cleanInput($_POST['phoneNumber']);
+    $address = cleanInput($_POST['address']);
+    $email = cleanInput($_POST['email']);
+    $password = cleanInput($_POST['password']);
+
+    //all patterns needed
+    $patternPass = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$^";
+    $patternEmail;// needs to be done
+
+    //Tentative to change
+    if(empty($firstname)) {
+      $errors['firstname'] = "First Name Input Required";
+    }
+    if(empty($lastname)) {
+        $errors['lastname'] = "Last Name Input Required";
+      }
+    if(empty($phoneNumber)) {
+        $errors['phoneNumber'] = "Phone Number Input Required";
+    }
+    if(empty($address)) {
+        $errors['address'] = "Address Input Required";
+      }
+    if(empty($email)) {
+        $errors['email'] = "Email Input Required";
+    }
+    if(empty($password)) {
         $errors['password'] = "Password Input Required";
     }
-    elseif(!preg_match($pattern, $cleanPassword)){
+    elseif(!preg_match($pattern, $password)){
         $errors['password'] = "Please follow Pasword formatting";
     }
     else
     $bool = true;
-       
-
-    
-  
     }
     ?>
