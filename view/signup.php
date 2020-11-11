184ed9c262832b6e24fbb4c17428493661541f8e
Jason Mui <Jasonmui22@gmail.com>
2020-11-11 14:29:44 -0500

    Need to be connected to frontend and Server

--- view/signup.php
+++ view/signup.php
@@ -1,8 +1,7 @@
 <?php
-
+    //need to include connect
     //Sessions needs to be added
-
-  $errors = [];
+    //Post submission
 
     function cleanInput($data){ //sanitize data 
         $data = trim($data);
@@ -10,24 +9,53 @@
         $data = htmlspecialchars($data);
         return $data;
     }
-
-	if(isset($_POST['submit'])){
     $firstname = cleanInput($_POST['firsname']);
     $lastname = cleanInput($_POST['lastname']);
     $phoneNumber = cleanInput($_POST['phoneNumber']);
     $address = cleanInput($_POST['address']);
     $email = cleanInput($_POST['email']);
     $password = cleanInput($_POST['password']);
+    $passwordCHECK = cleanInput($_POST['passwordCHECK']);
 
     //all patterns needed
     $patternPass = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$";
     //Minimum eight characters, at least one letter, one number and one special character:
     
-    $patternUsername = "^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$";
-    //5 to 20 characers
-    //all alphanumeric
-    //allow (.), (_), (-), cant be at begining or end, no consecutive
+    //UPdate Querey
+    $queryCheck = "SELECT ... FROM ... WHERE ... = ? LIMIT 1";
+    $query = "INSERT";
 
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL))&&!preg_match($patternUsername)
-   
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    //invalid email
+    }
+    else if($password != $passwordCHECK){
+    //Passwords do not match
+    }
+    else if(!preg_match($patternPass, $password)){
+    //Password do not have correct format
+    }
+    else{
+      //Checking if Aleady signed up
+      $stmt = $conn->prepare()
+      $stmt->bind_param("s", $email);
+      $stmt->execute();
+      $stmy->bind_result($email);
+      $stmt->store_result();
+      if($stmt->num_rows>0){
+        //Aready Sign UP
+      }
+      else{
+      $stmt = $conn->prepare($query); 
+      //Order can be changed
+      $stmt->bind_param("ssssss", $firstname, $lastname, $phoneNumber, $phoneNumber, $address, $email, $password);
+      if($stmt->execute()){
+        //Sucessful
+      }
+      else{
+        //Unsuccessful
+      }
+
+
+      //SESSIONS stuff
+    }
     ?>
