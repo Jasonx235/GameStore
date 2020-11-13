899620895824388ad6370f2bb42a1356350f17c0
Jason Mui <Jasonmui22@gmail.com>
2020-11-13 14:47:54 -0500

    DEV: updated login and update, Still needs to be tested

--- view/signin.php
+++ view/signin.php
@@ -13,32 +13,36 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $data = htmlspecialchars($data);
         return $data;
     }
-
+    $loginQuery = "SELECT email, password FROM users WHERE email = ? AND password = ? LIMIT 1";
     $email = cleanInput(_POST['email']);
     $password = cleanInput(_POST['password']);
 
-    //Checking if any results exist
-    $loginQuery= "SELECT email, password FROM users WHERE email = ? AND password = ? LIMIT 1";
+    if(empty($email)){
+        $errors['email'] = "Username Required";
+    }
+    if(empty($password)){
+        $errors['password'] = "Password Required";
+    }
+
+    //if no errors check if credentials are valid
+    if(count($errors)===0){
+
     $stmt = $conn->prepare($loginQuery);
     $stmt = bind_param('ss', $email, $password);
     $stmt = store_result();
-    if($stmt->num_rows >0){ 
-        if($stmt->fetch()){
-            $checkQuery = "SELECT user_id FROM users WHERE email = '". $email . "'";
-            $stmt = $conn->prepare($checkQuery);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            $return = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-            if($result){
-                foreach($result as $r){
-                    $_SESSION['id'] = $r['user_id'];
-                }
-                $_SESSION['email'] = $email;
-                $_SESSION['source'] = "login";
-                header("Location:profile.php");
-                exit();
-            }
-        }
+    $return = $stmt->get_result();
+    $user = $return->fetch_assoc();
+
+    $hash_pass = $user['password'];
+    $user_id = $user['id'];
+    //Verifying Email
+    if(password_verify($password, $hash_pass)){ 
+    
+        $_SESSION['id'] = $user_id;
+        $_SESSION['email'] = $email;
+        $_SESSION['source'] = "login";
+        header("Location:profile.php");
+        exit();       
     }
     else{
         $error['invalidEP'] = "Invalid Email or Invalid Password or Invali Email and Password!"
